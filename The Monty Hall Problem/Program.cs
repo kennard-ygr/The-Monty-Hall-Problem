6a763549a3e47bb68ffecf982bb0f90e26df9073
Christopher Dollinger <chrismdollinger@gmail.com>
2022-04-24 11:10:59 -0400

    Add project files.

---
+++ The Monty Hall Problem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+static sbyte pickDoor(int doorsCount, int[] exclusions)
+{
+    sbyte door = (sbyte)new Random().Next(0, doorsCount);
+    while (exclusions.Contains(door))
+    {
+        door = (sbyte)new Random().Next(0, doorsCount);
+    }
+    return door;
+}
+
+const sbyte numDoors = 3;
+const int runs = 1000;
+sbyte[] doors = new sbyte[numDoors];
+sbyte[] exclusions = new sbyte[numDoors - 2];
+sbyte contestant;
+
+Console.WriteLine("Welcome to Let's make a deal!");
